4f1183281cea6a5ea9ce7dd6017295de2d2205dd
Cong_Huy <195303514+Williamzo1@users.noreply.github.com>
2026-03-22 22:02:08 +0700

    Upload 2951 problem

---
+++ Leetcode_Sol/Find_the_Peaks_2951.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+class Find_the_Peaks_2951 {
+    public List<Integer> findPeaks(int[] mountain) {
+        List<Integer> list = new ArrayList<>();
+        for (int i=1;i<mountain.length-1;i++){
+            if (mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]){
+                list.add(i);
+            }
+        }
+        return list;
+    }
+}
