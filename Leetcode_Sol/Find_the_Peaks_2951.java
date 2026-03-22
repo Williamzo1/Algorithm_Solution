397b58b5ec751ece53a763a4cf4b019714a5dc37
Cong_Huy <195303514+Williamzo1@users.noreply.github.com>
2026-03-22 22:03:28 +0700

    Correct library

--- Leetcode_Sol/Find_the_Peaks_2951.java
+++ Leetcode_Sol/Find_the_Peaks_2951.java
@@ -1,9 +1,11 @@
+import java.util.List;
 import java.util.ArrayList;
+
 class Find_the_Peaks_2951 {
     public List<Integer> findPeaks(int[] mountain) {
         List<Integer> list = new ArrayList<>();
-        for (int i=1;i<mountain.length-1;i++){
-            if (mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]){
+        for (int i = 1; i < mountain.length - 1; i++) {
+            if (mountain[i] > mountain[i - 1] && mountain[i] > mountain[i + 1]) {
                 list.add(i);
             }
         }
